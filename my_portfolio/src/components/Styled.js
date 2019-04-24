452fffe1a1422324979d0d4f17c87521dcaddcb5
cocoitali <cmburatto@gmail.com>
2019-04-24 12:42:53 -0600

    SideBar : created basic fixed sidebar

---
+++ my_portfolio/src/components/Styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+//SideBar.js
+export const SideBarWrapper = styled.div`
+  width: 20%;
+  background: #F3F4F8;
+  height: 100vh;
+`;
+
---
+++ my_portfolio/src/components/sidebar/SideBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    SideBarWrapper,
+  } from "../Styled";
+
+class SideBar extends Component {
+    render() {
+      return (
+        <SideBarWrapper>
+        </SideBarWrapper>
+      );
+    }
+  }
+
+export default SideBar;
