dccdb29d0bd7c022cd6deacefd3268f77cadddb0
cocoitali <cmburatto@gmail.com>
2019-04-24 16:27:13 -0600

    SideBar : sidebar links style and positioning

--- my_portfolio/src/components/Styled.js
+++ my_portfolio/src/components/Styled.js
@@ -6,16 +6,24 @@ export const SideBarWrapper = styled.div`
   width: 20%;
   background: #F3F4F8;
   height: 100vh;
+  margin-left: 10%;
+  overflow-y: auto;
+  display: flex;
+  justify-content: center;
 `;
 
 export const NavLinksWrapper = styled.div`
+margin-top: 15px;
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
+ 
 `
-
 export const SideBarLink = styled.div`
+    margin:5px;
     :hover {
         color: #0d98ba;
+        cursor: default;
+        text-decoration: underline;
     }
+
 `
